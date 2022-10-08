2049de958268e8dbe3a308320368cd45468d6692
DJ <subhasdheeraj@gmail.com>
2022-10-08 12:55:32 +0200

    multiplied bar

--- quick_compare/lib/main.dart
+++ quick_compare/lib/main.dart
@@ -30,17 +30,31 @@ class _QuickCompareState extends State<QuickCompare> {
     return Scaffold(
       appBar: AppBar(title: Text("Quick Compare")),
       body: Column(
-        children: [
+        children: <Widget>[
           Expanded(
             child: ListView.builder(
-              itemCount: 3,
+              itemCount: 10,
               itemBuilder: (context, index) {
-                return Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Container(color: Colors.green,child: Text(""),width: MediaQuery.of(context).size.width/3,),
-                    Container(color: Colors.yellow,child: Text(""),width: MediaQuery.of(context).size.width/3,),
-                    Container(color: Colors.red,child: Text(""),width:MediaQuery.of(context).size.width/3,)
+                return Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text("Steckdose ${index+1}",style: TextStyle(fontSize: 25.0),),
+                    Divider(height: 5.0,color: Colors.transparent,),
+                    Stack(
+                      children:<Widget>[
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Container(color: Colors.green,child: Text(""),width: MediaQuery.of(context).size.width/3,height: 50,),
+                            Container(color: Colors.yellow,child: Text(""),width: MediaQuery.of(context).size.width/3,height: 50,),
+                            Container(color: Colors.red,child: Text(""),width:MediaQuery.of(context).size.width/3,height: 50,),
+                          ],
+                        ),
+                      ]
+                    ),
+                    Divider(height: 40.0,color: Colors.transparent,),
                   ],
                 );
               },
